cb88a727f2d68a57d079caaa26d41fd4ef343a0f
Ramiro Ivan Morales Pilaguano <111449010+ramimorales1986@users.noreply.github.com>
2026-06-27 19:17:48 -0500

    Enhance Streamlit app with sidebar and inputs
    Added sidebar elements for user input and calculation.

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,18 @@
 import streamlit as st
  
 st.title("Aplicación de Fundamentos Streamlit")
+ 
+st.sidebar.image("LogoUHE.png")
+ 
+st.sidebar.title("Parámetros")
+ 
+ 
+monto = st.number_input("Ingrese el monto de su prestamo")
+ 
+interes = st.number_input("Ingrese el interes")
+ 
+tiempo_meses = st.number_input("Ingrese el tiempo en meses")
+ 
+resultado = monto*interes*(tiempo_meses/12)
+ 
+st.write("El resultado es:" , resultado)
